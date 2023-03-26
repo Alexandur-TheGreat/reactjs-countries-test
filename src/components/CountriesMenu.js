01f68616696ca83234e568941fb1af7b7c61c36a
Alex Povhinrhaj <ppovhinrhaj@yahoo.com.au>
2023-03-26 20:01:10 +0800

    Table. Table Search and Saerch by Region complete

--- src/components/CountriesMenu.js
+++ src/components/CountriesMenu.js
@@ -1,14 +1,107 @@
 import { useState, useEffect } from "react";
 import axios from 'axios';
 import Country from "./Country";
+import { Table, Switch,Input } from 'antd';
 
 const baseURL = "https://restcountries.com/v3.1/all";
 
 export default function CountriesMenu() {
 
     const [countries, setCountries] = useState([]);
-    const [searchText, setSearchText] = useState("");
     const [countriesFiltered, setCountriesFiltered] = useState([]);
+    const [switchChecked, setSwitchChecked] = useState(true);
+    const [searchRegion, setSearchRegion] = useState("");
+    const [searchText, setSearchText] = useState("null");
+
+    const columns = [
+        {
+            title: "Image",
+            dataIndex: "svg",  // this is the value that is parsed from the DB / server side
+            render: (text, record) => {
+                return (
+                    <div>
+                        <img src={record.flags.svg} />
+                    </div>
+                );
+            },
+            width: '20%'
+        },
+        {
+            title: 'Population',
+            dataIndex: 'population',
+            render: (text, record) => {
+                return (
+                    <div>
+                        {record.population.toLocaleString()}
+                    </div>
+                );
+            },
+        },
+        {
+            title: 'Region',
+            dataIndex: 'region',
+            filters: [
+                {
+                    text: 'Africa',
+                    value: 'Africa',
+                },
+                {
+                    text: 'Americas',
+                    value: 'Americas',
+                },
+                {
+                    text: 'Asia',
+                    value: 'Asia',
+                },
+                {
+                    text: 'Europe',
+                    value: 'Europe',
+                },
+                {
+                    text: 'Ocenia',
+                    value: 'Ocenia',
+                },
+            ],
+            filteredValue: [searchRegion],
+            onFilter: (value, record) => {
+                return String(record.region).toLowerCase().includes(value.toLowerCase());
+            },
+        },
+        {
+            title: 'Capital',
+            dataIndex: 'capital',
+        },
+        {
+            title: 'Country Name',
+            dataIndex: 'name',
+            render: (text, record) => {
+                return (
+                    <div>
+                        {record.name.common}
+                    </div>
+                );
+            },
+            filteredValue: [searchText],
+            onFilter: (value, record) => {console.log("Record",record.name.common)
+            console.log("Record 2 " ,value)
+                return record.name.common.includes(value);
+            },
+        },
+        {
+            title: 'Currency',
+            dataIndex: 'currency',
+            render: (text, record) => {
+                return (
+                    <div>
+                        {Object.values(record.currencies)
+                            .map((currency) => currency.name)
+                            .join(', ')}
+                    </div>
+                );
+            },
+
+        },
+    ];
 
     useEffect(() => {
         const countriesList = async () => {
@@ -31,68 +124,71 @@ export default function CountriesMenu() {
     const regions = [
         { name: "Africa" }, { name: "Americas" }, { name: "Asia" }, { name: "Europe" }, { name: "Ocenia" }];
 
-    async function searchCountry() {
-        try {
-            const res = await fetch(
-                `https://restcountries.com/v3.1/name/${searchText}`
-            );
-            const data = await res.json();
-            setCountriesFiltered(data);
-        } catch (error) {
-            console.error(error);
-        }
-    }
-
     async function filterByRegion(searchRegion) {
         try {
-            // const res = await fetch(
-            //     `https://restcountries.com/v3.1/region/${region}`
-            // );
-            // const data = await res.json();
-            // setCountries(data);
-            console.log("Original table",countries)
-
-            const filteredData = countries.filter(
-                (x) => x.region == searchRegion
-              );
-
-              console.log("Region searched",filteredData)
-              setCountries(filteredData);
-              
-
+            setSearchRegion(searchRegion);
         } catch (error) {
             console.error(error);
         }
     }
 
-    // const filterByRegion = (region, searchParams) => {
-    //     return countries.filter(({ name, region }) => {
-    //             const testString = `${author}${title}`.toLowerCase();
-    //           let authorName = author.toLowerCase();
-    //       let bookTitle = title.toLowerCase();
-    //             return testString.includes('m') && (authorName.startsWith('m') || bookTitle.startsWith('m'));
-    //           })
-    //   }
+    const onChangeSwitch = (checked) => {
+        console.log(`switch to ${checked}`);
+        setSwitchChecked(checked);
+    };
 
-    function handleSearchCountry(e) {
-        console.log("MASUK HERE");
-        e.preventDefault();
-        searchCountry();
-    }
-
+    const onTableChange = (pagination, filters, sorter, extra) => {
+        console.log('params', pagination, filters, sorter, extra);
+    };
 
     return (<>
-        {!countries ? (<h1>Laoding...</h1>) :
-            (
-                <section className="container mx-auto p-8">
+        {!switchChecked ? (
+            <section className="container mx-auto p-8">
+                <Switch checkedChildren="Card" unCheckedChildren="Table" onChange={onChangeSwitch} defaultChecked />
 
+                <div className="flex flex-col gap-4 md:flex-row md:items-center md:justify-between mb-8">
+                    <h1>Table View</h1>
+                        <Input.Search
+                            placeholder="Search for a country"
+                            onSearch={(value)=>{
+                                setSearchText(value)
+                            }}
+                            className="py-3 px-4 text-gray-600 placeholder-gray-600 w-full shadow rounded outline-none transition-all duration-200"
+                        />
+                    <form>
+                        <select
+                            name="filter-by-region"
+                            id="filter-by-region"
+                            className="w-52 py-3 px-4 outline-none shadow rounded text-gray-600"
+                            value={regions.name}
+                            onChange={(e) => filterByRegion(e.target.value)}
+                        >
+                            {regions.map((region, index) => (
+                                <option key={index} value={region.name}>
+                                    {region.name}
+                                </option>
+                            ))}
+                        </select>
+                    </form>
+                </div>
+                <Table columns={columns} dataSource={countries} onChange={onTableChange} />
+            </section>
+
+
+        )
+
+            :
+            (
+
+                <section className="container mx-auto p-8">
+                    <Switch checkedChildren="Card" unCheckedChildren="Table" onChange={onChangeSwitch} defaultChecked className="background-color:black" />
                     <div className="flex flex-col gap-4 md:flex-row md:items-center md:justify-between mb-8">
+                        <h1>Card View</h1>
                         <form
-                            onSubmit={handleSearchCountry}
                             autoComplete="off"
                             className="max-w-4xl md:flex-1"
                         >
-                            <input
+                            {/* <input
                                 type="text"
                                 name="search"
                                 id="search"
@@ -101,7 +197,7 @@ export default function CountriesMenu() {
                                 value={searchText}
                                 onChange={(e) => setSearchText(e.target.value)}
                                 className="py-3 px-4 text-gray-600 placeholder-gray-600 w-full shadow rounded outline-none transition-all duration-200"
-                            />
+                            /> */}
                         </form>
 
                         <form>
@@ -120,9 +216,8 @@ export default function CountriesMenu() {
                             </select>
                         </form>
                     </div>
-
                     <div className="background-color:whitesmoke grid grid -cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
-                        {countries.length>0 && countries.map((country) => (
+                        {countries.length > 0 && countries.map((country) => (
                             <Country key={country.name.common}{...country} />
                         ))}
                     </div>
